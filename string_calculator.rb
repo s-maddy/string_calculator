d8e32a069d94a87d476f283d20f8fa9c61b5ed68
s-maddy <shekhar.maddy1993@gmail.com>
2025-03-29 15:37:11 +0530

    new method for negative numbers added and raise error if number exists

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,7 +1,17 @@
 module StringCalculator
   def add
     return 0 if empty?
+    raise_negative_numbers_error
     numbers.reduce(0, :+)
+  end
+
+  def raise_negative_numbers_error
+    return if negative_numbers.empty?
+    raise "negatives not allowed: #{negative_numbers.join(', ')}"
+  end
+
+  def negative_numbers
+    numbers.select { |n| n < 0 }
   end
 
   def numbers
