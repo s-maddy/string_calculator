b01886d7473d6567961ad94aba12cad889779898
s-maddy <shekhar.maddy1993@gmail.com>
2025-03-29 14:45:37 +0530

    test for single number added

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,15 @@ RSpec.describe StringCalculator do
     it "returns 0 for an empty string" do
       "".extend(StringCalculator).add.should == 0
     end
+
+    context "with a single number" do
+      it "returns the number 0 if 0" do
+        "0".extend(StringCalculator).add.should == 0
+      end
+
+      it "returns the number 1 if 1" do
+        "1".extend(StringCalculator).add.should == 1
+      end
+    end
   end
 end
